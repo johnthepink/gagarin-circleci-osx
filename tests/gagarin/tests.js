ceb53acb2b1f869c65023937abc15687ba19c376
John Pinkerton <jpinkerton88@gmail.com>
2015-12-22 15:34:10 -0500

    Updates for osx containers on circle

--- tests/gagarin/tests.js
+++ tests/gagarin/tests.js
@@ -4,3 +4,18 @@ describe('My first Gagarin test suite', function () {
     return server.execute(function () { console.log('I am alive!'); });
   });
 });
+
+describe('You can also use browser in your tests', function () {
+  var server = meteor();
+  var client = browser(server);
+
+  it('should work for both client and server', function () {
+    return client.execute(function () {
+      // some code to execute
+    }).then(function () {
+      return server.execute(function () {
+        // some code to execute on server
+      });
+    });
+  });
+});
